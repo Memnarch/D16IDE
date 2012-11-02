ef7fcdf7f37fe217a4d4ce3366fecbb911501153
Memnarch <Sebal007@freenet.de>
2012-11-02 14:52:29 +0100

    extended TSImpleRefactor to auoclose begin end blocks based on cursorposition.

--- IDEEdit.pas
+++ IDEEdit.pas
@@ -9,8 +9,10 @@ type
   TIDEEdit = class(TSynEdit)
   private
     FRefactor: TSimpleRefactor;
+    procedure InternalKeyPress(Sender: TObject; var Key: Char);
   public
     constructor Create(AOwner: TComponent); override;
+    destructor Destroy(); override;
     property Refactor: TSimpleRefactor read FRefactor;
   end;
 
@@ -24,7 +26,7 @@ uses
 constructor TIDEEdit.Create(AOwner: TComponent);
 begin
   inherited;
-  FRefactor := TSimpleRefactor.Create(Lines);
+  FRefactor := TSimpleRefactor.Create(Self);
   Highlighter := TSynPasSyn.Create(Self);
   TSynPasSyn(Highlighter).AsmAttri.Foreground := clBlack;
   TSynPasSyn(Highlighter).KeyAttri.Foreground := clNavy;
@@ -35,6 +37,21 @@ begin
   Options := Options - [eoSmartTabs];
   DoubleBuffered := True;
   SearchEngine := TSynEditSearch.Create(Self);
+  OnKeyPress := InternalKeyPress;
+end;
+
+destructor TIDEEdit.Destroy;
+begin
+  FRefactor.Free;
+  inherited;
+end;
+
+procedure TIDEEdit.InternalKeyPress(Sender: TObject; var Key: Char);
+begin
+  if Key = #13 then
+  begin
+    FRefactor.CompleteBlocks();
+  end;
 end;
 
 end.
--- SimpleRefactor.pas
+++ SimpleRefactor.pas
@@ -3,27 +3,76 @@ unit SimpleRefactor;
 interface
 
 uses
-  Classes, Types, SysUtils;
+  Classes, Types, SysUtils, SynEdit;
 
 type
   TSimpleRefactor = class
   private
-    FSourceLink: TStrings;
+    FEdit: TSynEdit;
   public
-    constructor Create(ASourceLink: TStrings);
+    constructor Create(AEdit: TSynEdit);
     procedure RenameHeader(AName: string);
+    procedure CompleteBlocks();
   end;
 
 implementation
 
 uses
-  StrUtils;
+  StrUtils, Lexer, Token;
 
 { TSimpleRefactor }
 
-constructor TSimpleRefactor.Create(ASourceLink: TStrings);
+procedure TSimpleRefactor.CompleteBlocks;
+var
+  LLexer: TLexer;
+  LToken, LOpenToken: TToken;
+  LOpen, LClose, LIdent: Integer;
+  LText: string;
 begin
-  FSourceLink := ASourceLink;
+  LOpen := 0;
+  LClose := 0;
+  LOpenToken := nil;
+  LLexer := TLexer.Create();
+  try
+    LLexer.LoadFromString(FEdit.Text);
+    while not LLexer.EOF do
+    begin
+      LToken := LLexer.GetToken();
+      if LToken.IsType(ttReserved) then
+      begin
+        if AnsiIndexText(LToken.Content, ['unit', 'asm', 'begin']) > -1 then
+        begin
+          Inc(LOpen);
+          if LToken.FoundInLine < FEdit.CaretY then
+          begin
+            LOpenToken := LToken;
+          end;
+        end
+        else
+        begin
+          if SameText(LToken.Content, 'end') then
+          begin
+            Inc(LClose);
+          end;
+        end;
+      end;
+    end;
+    if (LOpen > LClose) and (FEdit.CaretY > 0) and Assigned(LOpenToken) then
+    begin
+      FEdit.LInes.Strings[FEdit.CaretY-1] := FEdit.Lines.Strings[FEdit.CaretY-1] + #9; //jump into nextline with increased ident
+      FEdit.CaretX := FEdit.CaretX + 1;
+      LText := FEdit.Lines.Strings[LOpenToken.FoundInLine];
+      LIdent := (Length(LText) - Length(TrimLeft(LText))) - 1;
+      FEdit.Lines.Insert(FEdit.CaretY, StringOfChar(' ', LIdent) + 'end;');
+    end;
+  finally
+    LLexer.Free;
+  end;
+end;
+
+constructor TSimpleRefactor.Create(AEdit: TSynEdit);
+begin
+  FEdit := AEdit;
 end;
 
 procedure TSimpleRefactor.RenameHeader(AName: string);
@@ -31,16 +80,16 @@ var
   i, LPos: Integer;
   LEnd: string;
 begin
-  for i := 0 to FSourceLink.Count - 1 do
+  for i := 0 to FEdit.Lines.Count - 1 do
   begin
-    if StartsText('unit ', Trim(FSourceLink.Strings[i])) then
+    if StartsText('unit ', Trim(FEdit.Lines.Strings[i])) then
     begin
-      LPos := Pos(';', FSourceLink.Strings[i]);
+      LPos := Pos(';', FEdit.Lines.Strings[i]);
       if LPos > 0 then
       begin
-        LEnd := copy(FSourceLink.Strings[i], LPos + 1, Length(FSourceLink.Strings[i]));
+        LEnd := copy(FEdit.Lines.Strings[i], LPos + 1, Length(FEdit.Lines.Strings[i]));
       end;
-      FSourceLink.Strings[i] := 'unit ' + AName + ';' + LEnd;
+      FEdit.Lines.Strings[i] := 'unit ' + AName + ';' + LEnd;
       Break;
     end;
   end;
