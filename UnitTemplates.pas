94630e5c116f5d743ee392e0df25c625bcd37ce7
Memnarch <Sebal007@freenet.de>
2012-12-15 17:17:12 +0100

    added programunit-template

--- UnitTemplates.pas
+++ UnitTemplates.pas
@@ -9,6 +9,9 @@ const
   CDefaultUnit = 'unit Name;' + sLineBreak + sLineBreak + sLineBreak +
     sLineBreak + sLineBreak + sLineBreak + 'end.';
 
+  CDefaultProgramUnit = 'program Name;' + sLineBreak + sLineBreak + sLineBreak +
+    sLineBreak + sLineBreak + 'begin' + sLineBreak + sLineBreak + 'end.';
+
 implementation
 
 end.
