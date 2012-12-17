9da7383cf4a023885b1b79b383302c088c046f71
Memnarch <Sebal007@freenet.de>
2012-12-18 00:04:50 +0100

    LineInfo now includes a LineMapping

--- LineInfo.pas
+++ LineInfo.pas
@@ -3,7 +3,7 @@ unit LineInfo;
 interface
 
 uses
-  Classes, Types;
+  Classes, Types, LineMapping;
 
 type
   TLineState = (ltNone, ltModified, ltSaved);
@@ -11,9 +11,14 @@ type
   TLineInfo = class
   private
     FState: TLineState;
+    FMapping: TLineMapping;
+    function GetIsPossibleBreakPoint: Boolean;
   public
     constructor Create(AState: TLineState);
+    destructor Destroy(); override;
     property State: TLineState read FState write FState;
+    property Mapping: TLineMapping read FMapping;
+    property IsPossibleBreakPoint: Boolean read GetIsPossibleBreakPoint;
   end;
 
 implementation
@@ -25,6 +30,18 @@ implementation
 constructor TLineInfo.Create(AState: TLineState);
 begin
   FState := AState;
+  FMapping := TLineMapping.Create();
+end;
+
+destructor TLineInfo.Destroy;
+begin
+  FMapping.Free;
+  inherited;
+end;
+
+function TLineInfo.GetIsPossibleBreakPoint: Boolean;
+begin
+  Result := (FMapping.ElementName = '') and (FMapping.MemoryAddress >= 0);
 end;
 
 end.
