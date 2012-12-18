1f001d04ec15e4a5ff74dd9146f347be4b644e23
Memnarch <Sebal007@freenet.de>
2012-12-18 22:35:12 +0100

    unitmapping now includes a list of breakpoints which is kept even when the linemapping is cleared(i.e when closing/reopening a unit)

--- UnitMapping.pas
+++ UnitMapping.pas
@@ -3,19 +3,20 @@ unit UnitMapping;
 interface
 
 uses
-  Classes, Types, Generics.Collections, LineMapping;
+  Classes, Types, Generics.Collections, LineMapping, BreakPoint;
 
 type
   TUnitMapping = class
   private
     FD16UnitName: string;
     FMapping: TObjectList<TLineMapping>;
-
+    FBreakPoints: TObjectList<TBreakPoint>;
   public
     constructor Create();
     destructor Destroy(); override;
     property D16UnitName: string read FD16UnitName write FD16UnitName;
     property Mapping: TObjectList<TLineMapping> read FMapping;
+    property BreakPoints: TObjectList<TBreakPoint> read FBreakPoints;
   end;
 
 implementation
@@ -25,11 +26,13 @@ implementation
 constructor TUnitMapping.Create;
 begin
   FMapping := TObjectList<TLineMapping>.Create();
+  FBreakPoints := TObjectList<TBreakPoint>.Create();
 end;
 
 destructor TUnitMapping.Destroy;
 begin
   FMapping.Free;
+  FBreakPoints.Free;
   inherited;
 end;
 
