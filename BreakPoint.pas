6c1fc046fc7de7b6348915267b4f749e4bc8d55d
Memnarch <Sebal007@freenet.de>
2012-12-20 22:33:14 +0100

    a breakpoint now contains the unitname where its placed, too

--- BreakPoint.pas
+++ BreakPoint.pas
@@ -13,7 +13,9 @@ type
     FMemory: Integer;
     FState: TBreakPointState;
     FUnitLine: Integer;
+    FD16UnitName: string;
   public
+    property D16UnitName: string read FD16UnitName write FD16UnitName;
     property UnitLine: Integer read FUnitLine write FUnitLine;
     property Memory: Integer read FMemory write FMemory;
     property State: TBreakPointState read FState write FState;
