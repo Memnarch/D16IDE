a8908dd9c3461a5819d86eb16663f44247b6414d
Memnarch <Sebal007@freenet.de>
2013-03-17 14:02:03 +0100

    CodeTree now respects interface and implementation section visibility

--- CodeTreeController.pas
+++ CodeTreeController.pas
@@ -3,7 +3,7 @@ unit CodeTreeController;
 interface
 
 uses
-  Classes, Types, VirtualTrees, PascalUnit;
+  Classes, Types, Generics.Collections, VirtualTrees, PascalUnit, CodeElement;
 
 type
   TCodeNodeData = record
@@ -16,6 +16,12 @@ type
   TCodeTreeController = class
   private
     FTree: TVirtualStringTree;
+    FUses: PVirtualNode;
+    FTypes: PVirtualNode;
+    FVars: PVirtualNode;
+    FProcedures: PVirtualNode;
+    procedure CreateSectionNodes();
+    procedure BuildCodeTreeFromElements(AElements: TObjectList<TCodeElement>);
   public
     constructor Create(ATree: TVirtualStringTree);
     procedure BuildCodeTreeFromUnit(AUnit: TPascalUnit);
@@ -24,52 +30,33 @@ type
 implementation
 
 uses
-  CodeElement, VarDeclaration, ProcDeclaration, DataType;
+  VarDeclaration, ProcDeclaration, DataType;
 
 { TFTreeController }
 
-procedure TCodeTreeController.BuildCodeTreeFromUnit(AUnit: TPascalUnit);
+procedure TCodeTreeController.BuildCodeTreeFromElements(
+  AElements: TObjectList<TCodeElement>);
 var
-  LUses, LProcedures, LVars, LTypes, LNode: PVirtualNode;
+  LNode: PVirtualNode;
   LElement: TCodeElement;
-  LName: string;
   LPreFix: string;
 begin
-  FTree.BeginUpdate;
-  FTree.Clear;
-  LUses := FTree.AddChild(nil);
-  PCodeNodeData(FTree.GetNodeData(LUses)).Caption := 'Uses';
-  PCodeNodeData(FTree.GetNodeData(LUses)).Line := -1;
-  LVars := FTree.AddChild(nil);
-  LTypes := FTree.AddChild(nil);
-  PCodeNodeData(FTree.GetNodeData(LTypes)).Caption := 'Types';
-  PCodeNodeData(FTree.GetNodeData(LTypes)).Line := -1;
-  PCodeNodeData(FTree.GetNodeData(LVars)).Caption := 'Vars';
-  PCodeNodeData(FTree.GetNodeData(LVars)).Line := -1;
-  LProcedures := FTree.AddChild(nil);
-  PCodeNodeData(FTree.GetNodeData(LProcedures)).Caption := 'Procedures';
-  PCodeNodeData(FTree.GetNodeData(LProcedures)).Line := -1;
-  for LName in AUnit.UsedUnits do
-  begin
-      LNode := FTree.AddChild(LUses);
-      PCodeNodeData(FTree.GetNodeData(LNode)).Caption := LName;
-  end;
-  for LElement in AUnit.SubElements do
+  for LElement in AElements do
   begin
     LNode := nil;
     if LElement is TVarDeclaration then
     begin
-      LNode := FTree.AddChild(LVars);
+      LNode := FTree.AddChild(FVars);
       PCodeNodeData(FTree.GetNodeData(LNode)).Caption := LElement.Name + ': ' + TVarDeclaration(LElement).DataType.Name;
     end;
-    if LElement is TProcDeclaration then
+    if (LElement is TProcDeclaration) and (not TProcDeclaration(LElement).IsDummy) then
     begin
-      LNode := FTree.AddChild(LProcedures);
+      LNode := FTree.AddChild(FProcedures);
       PCodeNodeData(FTree.GetNodeData(LNode)).Caption := LElement.Name;
     end;
     if (LElement is TDataType) and (TDataType(LElement) <> TDataType(LElement).BaseType) then
     begin
-      LNode := FTree.AddChild(LTypes);
+      LNode := FTree.AddChild(FTypes);
       LPreFix := '';
       if TDataType(LElement).RawType = rtPointer then
       begin
@@ -86,6 +73,23 @@ begin
       PCodeNodeData(FTree.GetNodeData(LNode)).Line := LElement.Line;
     end;
   end;
+end;
+
+procedure TCodeTreeController.BuildCodeTreeFromUnit(AUnit: TPascalUnit);
+var
+  LNode: PVirtualNode;
+  LName: string;
+begin
+  FTree.BeginUpdate;
+  FTree.Clear;
+  CreateSectionNodes();
+  for LName in AUnit.UsedUnits do
+  begin
+    LNode := FTree.AddChild(FUses);
+    PCodeNodeData(FTree.GetNodeData(LNode)).Caption := LName;
+  end;
+  BuildCodeTreeFromElements(AUnit.SubElements);
+  BuildCodeTreeFromElements(AUnit.ImplementationSection);
   FTree.FullExpand(nil);
   FTree.EndUpdate;
 end;
@@ -93,6 +97,22 @@ end;
 constructor TCodeTreeController.Create(ATree: TVirtualStringTree);
 begin
   FTree := ATree;
+end;
+
+procedure TCodeTreeController.CreateSectionNodes;
+begin
+  FUses := FTree.AddChild(nil);
+  PCodeNodeData(FTree.GetNodeData(FUses)).Caption := 'Uses';
+  PCodeNodeData(FTree.GetNodeData(FUses)).Line := -1;
+  FVars := FTree.AddChild(nil);
+  FTypes := FTree.AddChild(nil);
+  PCodeNodeData(FTree.GetNodeData(FTypes)).Caption := 'Types';
+  PCodeNodeData(FTree.GetNodeData(FTypes)).Line := -1;
+  PCodeNodeData(FTree.GetNodeData(FVars)).Caption := 'Vars';
+  PCodeNodeData(FTree.GetNodeData(FVars)).Line := -1;
+  FProcedures := FTree.AddChild(nil);
+  PCodeNodeData(FTree.GetNodeData(FProcedures)).Caption := 'Procedures';
+  PCodeNodeData(FTree.GetNodeData(FProcedures)).Line := -1;
 end;
 
 end.
